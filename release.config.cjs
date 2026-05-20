944ede81c7c51d0e51b035741ffadf186dc6d77e
kitt.digital <169592437+kitt-digital@users.noreply.github.com>
2026-05-20 15:42:28 +0200

    fix(release): - bin dir in release config

--- release.config.cjs
+++ release.config.cjs
@@ -39,7 +39,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['README.md', 'CHANGELOG.md', 'package.json', 'src/@types/**/*.d.ts', 'bin/*.js'],
+        assets: ['README.md', 'CHANGELOG.md', 'package.json', 'src/@types/**/*.d.ts'],
         message: '[RELEASE]: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
     ]
