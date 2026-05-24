e2fd1e4bb3c0721b06bec2d30c104afe1c86cd25
kitt.digital <169592437+kitt-digital@users.noreply.github.com>
2026-05-24 18:59:45 +0200

    fix(release): ~ switch npm to pnpm

--- release.config.cjs
+++ release.config.cjs
@@ -35,7 +35,7 @@ module.exports = {
     ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@anolilab/semantic-release-pnpm',
+    'semantic-release-pnpm',
     [
       '@semantic-release/git',
       {
