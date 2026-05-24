638ef2d6fb24303e5fb13f291bc1585809d0d96e
kitt.digital <169592437+kitt-digital@users.noreply.github.com>
2026-05-24 08:27:38 +0200

    patch(vite): ~ optimized config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { copyFile } from 'fs';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
@@ -6,23 +7,36 @@ import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   cacheDir: 'cache/.vite',
+  // publicDir is used by storybook
+  publicDir: false,
   json: {
     stringify: true
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern'
-      }
-    }
   },
   plugins: [
     vue(),
     dts({
       outDirs: ['dist', 'src/@types'],
       cleanVueFileName: true,
-      staticImport: true
-    })
+      staticImport: true,
+      exclude: ['**/*.stories.ts']
+    }),
+    {
+      // custom plugin to call scripts after the build is finished
+      name: 'ClosePlugin',
+      // use this to catch the end of a build without errors
+      closeBundle(id) {
+        // copy the styles.css file from src to dist after the build is finished
+        copyFile(
+          resolve(__dirname, './src/style.css'),
+          resolve(__dirname, './dist/style.css'),
+          err => {
+            if (err) throw err;
+            console.log('File was copied to destination');
+          }
+        );
+        process.exit(0);
+      }
+    }
   ],
   resolve: {
     extensions: ['.md', '.mdx', '.ts', 'tsx', '.js', '.jsx', '.vue', '.json'],
@@ -33,17 +47,29 @@ export default defineConfig({
   build: {
     target: 'esnext',
     minify: true,
+    emptyOutDir: true,
     reportCompressedSize: true,
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
       fileName: 'index',
       formats: ['es']
     },
-    rollupOptions: {
+    cssCodeSplit: true,
+    rolldownOptions: {
       external: ['vue'],
       output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
         globals: {
           vue: 'Vue'
+        },
+        manualChunks: moduleId => {
+          if (moduleId.includes('components')) {
+            const modulePath = moduleId.match(/components\b\/.*?\.vue/g)?.[0];
+            return modulePath ? `${modulePath.replace(/\w+\.vue$/g, '')}index` : null;
+          }
+          return null;
         }
       }
     }
