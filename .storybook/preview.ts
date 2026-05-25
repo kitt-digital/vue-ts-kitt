357e52b3011f4791f91f1a6bcf8ff4399499bc0d
kitt.digital <169592437+kitt-digital@users.noreply.github.com>
2026-05-25 13:36:32 +0200

    patch(storybook): + layout centered

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import type { Preview } from '@storybook/vue3-vite';
 
 const preview: Preview = {
   parameters: {
+    layout: 'centered',
     controls: {
       matchers: {
         color: /(background|color)$/i,
